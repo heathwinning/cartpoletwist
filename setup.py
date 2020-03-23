61f1da13fd4ee9d0772471339ffda8a815bd23a3
Heath Winning <heathwinning@gmail.com>
2020-03-23 12:55:01 +1100

    fix packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_cartpoletwist',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym'],  # And any other dependencies foo needs
       packages=['gym_cartpoletwist', 'gym_cartpoletwist.envs']
 )
