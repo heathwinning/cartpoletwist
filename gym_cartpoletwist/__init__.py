9a2d640ef77203200f400d5e919e677dfe82a4c6
Heath Winning <heathwinning@gmail.com>
2020-03-23 12:47:18 +1100

    fix packaging

--- gym_cartpoletwist/__init__.py
+++ gym_cartpoletwist/__init__.py
@@ -3,6 +3,5 @@ from gym.envs.registration import register
 register(
     id='cartpoletwist-v0',
     entry_point='gym_cartpoletwist.envs:CartPoleTwistEnv',
-    max_episode_steps=200,
-
+    max_episode_steps=200
 )
