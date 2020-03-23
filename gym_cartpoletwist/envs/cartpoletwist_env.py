c39444406f7e3b2c9878391732eac36401acce3f
Heath Winning <heathwinning@gmail.com>
2020-03-23 13:22:32 +1100

    fix packaging

--- gym_cartpoletwist/envs/cartpoletwist_env.py
+++ gym_cartpoletwist/envs/cartpoletwist_env.py
@@ -59,10 +59,10 @@ class CartPoleTwistEnv(gym.Env):
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = 'euler'
 
-        self.desired_angle = 30 * math.pi / 360
+        self.desired_angle = 80 * math.pi / 360
 
         # Angle at which to fail the episode
-        self.theta_threshold_radians = 30 * 2 * math.pi / 360
+        self.theta_threshold_radians = 360 * 2 * math.pi / 360
         self.x_threshold = 2.4
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
